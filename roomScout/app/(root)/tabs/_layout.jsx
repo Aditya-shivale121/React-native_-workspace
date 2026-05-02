149def078ee072260bb1aa9c305d79a239814125
Aditya Shivale <Shivaleaditya22@gmail.com>
2026-05-02 11:22:42 +0530

    added auth to kribb

---
+++ roomScout/app/(root)/tabs/_layout.jsx
@@ -0,0 +1,28 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
+
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" androidSrc={<VectorIcon family={Ionicons} name="home" />} />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="search">
+        <Icon sf="magnifyingglass" androidSrc={<VectorIcon family={Ionicons} name="search" />} />
+        <Label>Search</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="saved">
+        <Icon sf="heart.fill" androidSrc={<VectorIcon family={Ionicons} name="heart" />} />
+        <Label>Saved</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="profile">
+        <Icon sf="person.fill" androidSrc={<VectorIcon family={Ionicons} name="person" />} />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
+}
